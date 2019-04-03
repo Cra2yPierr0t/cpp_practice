22bb22ff03dcb226c339397c2f2d23866580215f
pirrot <uchiyama.kazuhide@gmail.com>
2019-04-03 23:35:02 +0900

    ぎっはぶでこんなコード載せるの恥ずかしいと思わないの？？

--- syoshiki.cpp
+++ syoshiki.cpp
@@ -23,6 +23,28 @@ int main(){
     cout << "b = " << b << endl;
     cout.unsetf(ios::hex);
 
+    cout.setf(ios::oct,ios::basefield); // <- する必要無いやんけアホか
+    cout << "\t8進数表現" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    cout.setf(ios::dec,ios::basefield);
+
+    cout.width(20);
+    cout.fill('w');
+    cout.setf(ios::right,ios::adjustfield); // <- 右寄せに出来るらしい
+
+    cout << "a = " << a << endl;
+
+    double c = 0.00003939;
+
+    cout.setf(ios::fixed, ios::floatfield);
+    cout << "固定小数点表示\t: c = " << c << endl;
+
+    cout.setf(ios::scientific,ios::floatfield);
+    cout << "科学技術表示\t: c = " << c << endl;
+    
+
     return 0;
 
     }
